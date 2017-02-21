9ed955140e02bedefe1b40e170a47f20b21dc9ce
Christian Kirschberg <ckirschberg@gmail.com>
2017-02-21 10:41:46 +0100

    With our solution from class

--- Person.java
+++ Person.java
@@ -4,7 +4,7 @@ public class Person
 	private String firstname;
 	private String lastname;
 	private int age;
-	private Boolean male; //note: male=true means a male, male=false means a female
+	private Boolean male = true; //note: male=true means a male, male=false means a female
 						// (We learn a more intuitive way to do this later)
 	
   public Person(String firstname, String lastname, int age, Boolean male) {
@@ -18,49 +18,88 @@ public class Person
     this.lastname = lastname;
     this.age = age;
   }
+
+
+  // Get method that returns Something
+  public String getFirstname() {
+    return this.firstname;
+  }
 	
 	//The methods has to work no matter what the actual values
 	//are for the personobject in question!
 	
 	
 	//The idea here is we want an answer to the question:
-	//Is this persons age 15? (true or false?)
+	//Is this persons eg. age 15? (true or false?)
   //Print out "true" if the persons age is 15 or larger and "false" if not
 	public void isAge(int age)					
 	{
 		//Your code here.
-		
+    if (this.age >= age) {
+      System.out.println("isAge: " + true);
+    }
+    else {
+      System.out.println("isAge: " + false);
+    }
 	}
 
 	//Code method that prints "true" if name is the same as the parameter.
 	public void isPersonsName(String name)
 	{
-		
+    // Example that shows: It will not evaluate the second expression
+    // if the first is false.
+    if (name != null && name.equals(this.firstname + " " + this.lastname)) {
+      System.out.println("isPersonsName: " + true);
+    }
+    else {
+      System.out.println("isPersonsName: " + false);
+    }
 	}
 	
 	//Here we want a method that prints true if the name of the personobject is NOT the same
 	//as the parameter.
 	public void isPersonsNameNot(String name)
 	{
-		
+		if (!name.equals(this.firstname + " " + this.lastname)) {
+      System.out.println("isPersonsNameNot: " + true);
+    }
+    else {
+      System.out.println("isPersonsNameNot: " + false);
+    }
 	}
 	
 	//Not operator with other expression. Age must match and name must not match to print true
 	public void isPersonsNameNotButAgeIs(String name, int age)
 	{
-		
+		if (!name.equals(this.firstname + " " + this.lastname) && this.age == age) {
+      System.out.println("true");
+    }
+    else {
+      System.out.println(false);
+    }
 	}
 
 	//print true if name and age are the same as this person and otherwise false	
 	public void isPersonAgeAndName(String name, int age)
 	{
-		
+    if (name.equals(this.firstname + " " + this.lastname) && this.age == age) {
+      System.out.println("isPersonAgeAndName :" + true);
+    }
+    else {
+      System.out.println("isPersonAgeAndName :" + false);
+    }
 	}
 	
 	//Only print true if all 3 parameters (name, age and male) are the same as the person object
 	public void isPersonIdentical(String name, int age, boolean male)
 	{
-		
+		if (name.equals(this.firstname + " " + this.lastname) 
+      && this.age == age && this.male == male) {
+        System.out.println("IsPersonIdentical: " + true);
+    }
+    else {
+        System.out.println("IsPersonIdentical: " + false);
+    }
 	}
 	
 	
@@ -73,15 +112,31 @@ public class Person
 	//age between 76 and up: print "Very Old"
 	public void whichAgeGroup()
 	{
-		
+		if (this.age >= 0 && this.age <= 5) {
+      System.out.println("Small Child");
+    }
+    else if (this.age >= 6 && this.age <= 12) {
+      System.out.println("Child");
+    } // ... some if statements missing. Do it yourself!
+    else {
+      System.out.println("Something went wrong");
+    }
+    
+
 	}
 	
 
 	/////////////
 	// Extended / Difficult
 	////////////
-	public void isPersonsNameCaseInsensitive(String name)
+	public void isPersonsNameCaseInsensitive(String firstname, String lastname)
 	{
-		
+		if (this.firstname.equalsIgnoreCase(firstname) 
+      && this.lastname.equalsIgnoreCase(lastname)) {
+        System.out.println("Ignore Case: true");
+      }
+      else {
+        System.out.println("Ignore Case: false");
+      }
 	}
 }
--- RunPerson.java
+++ RunPerson.java
@@ -4,6 +4,22 @@ public class RunPerson
 	{
 		Person person1 = new Person("Peter", "Jensen", 12, true);
 	
+    person1.isAge(9);
+    person1.isPersonsName("Peter Jensen");
+    person1.isPersonsNameNot("Jonas Jensen");
+    person1.isPersonAgeAndName("Peter Jensen", 12);
+    person1.isPersonIdentical("Peter Jensen", 12, true);
+    person1.whichAgeGroup();
+    person1.isPersonsNameCaseInsensitive("peter", "Jensen");
+
+    // Use method that returns something
+    person1.getFirstname();
+
+    Person person3 = new Person("Peter", "Jensen", 12, true);
+    // person3.isPersonsName(null);
+
+
+
 		// Call the following methods and test that they work as you expect.		
 		// Call isAge(12) method
     // Call isPersonsName("Peter Jensen")
@@ -19,6 +35,6 @@ public class RunPerson
 		
 		
 		Person person2 = new Person("Tea", "Petersen", 42);
-    // Call isPersonIdentical("Tea Petersen", 42, false)
+    person2.isPersonIdentical("Tea Petersen", 42, false);
 	}
 }
